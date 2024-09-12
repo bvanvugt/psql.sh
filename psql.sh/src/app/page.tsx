f0ededeb83595d170506abe6f66b3974ac680ecc
Brad Van Vugt <1531419+bvanvugt@users.noreply.github.com>
2024-09-12 23:58:18 +0000

    commit

--- psql.sh/src/app/page.tsx
+++ psql.sh/src/app/page.tsx
@@ -1,4 +1,12 @@
-import Component from "./component";
+import dynamic from 'next/dynamic'
+
+// import Component from "../components/component";
+
+
+
+const Component = dynamic(() => import('../components/component'), {
+  ssr: false
+})
 
 export default function Home() {
 
